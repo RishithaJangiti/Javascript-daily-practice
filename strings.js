dcc564ad6453ebbdd7bfd1260cf2cadbfc9eaac3
RishithaJangiti <rishithajangiti23@gmail.com>
2026-01-07 21:39:19 +0530

    Initial commit

---
+++ strings.js
@@ -0,0 +1,13 @@
+//string:-is a sequence of characters
+//string can be created using single quotes(''),double quotes(""),backticks(``)
+let single='Rishitha';
+let double="Rishitha";
+let backticks=`Rishitha`;
+
+co="My name is";
+c="Rishitha";
+// 
+console.log(co+" "+c);
+
+console.log(`$${(5+3)}`);
+// ci=(`$${(5+3)}`);
